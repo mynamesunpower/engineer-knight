d65f7413d5257bb80ed4731b36a1f5b19c2791db
Taeyang Kim <mynamesunpower@gmail.com>
2021-09-01 15:38:45 +0900

    removed wrong test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,15 +3,15 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-  const linkElement = screen.getByText(/기사기사입니다/);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>,
+//   );
+//   const linkElement = screen.getByText(/기사기사입니다/);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 test('1 is 1', () => {
   expect(1).toBe(1);
