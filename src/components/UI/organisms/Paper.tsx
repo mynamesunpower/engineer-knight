fdb27fcb461b56e6c93a570df87ec36f32c16ea1
Taeyang Kim <mynamesunpower@gmail.com>
2021-09-02 15:24:33 +0900

    added react-code-blocks

--- src/components/UI/organisms/Paper.tsx
+++ src/components/UI/organisms/Paper.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from '@material-ui/core';
 import { solvedQuiz } from '../../../lib/quiz/QuizApi';
+import { monokai, CopyBlock } from 'react-code-blocks';
 
 const category1Map = new Map([
   ['00', '기타'],
@@ -25,6 +26,9 @@ const category1Map = new Map([
   ['11', '응용 SW 기초 기술 활용'],
   ['12', '제품소프트웨어 패키징'],
 ]);
+
+// TODO Client-side 에서 카테2 잡는게 맞는걸까?, 걍 조인해서 가져오면 안되나. -> QueryDSL
+const category2Map = new Map([]);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,8 +53,36 @@ type Props = {
   fetchQuiz: any;
 };
 
-const Paper = ({ quiz, quizLength, fetchQuiz }: Props) => {
+const Paper: React.FC<Props> = ({ quiz, quizLength, fetchQuiz }) => {
   const classes = useStyles();
+
+  // 언어
+  let language;
+  let quizContext;
+  let codes;
+
+  if (['08', '10'].includes(quiz.category1)) {
+    quizContext = quiz.quiz.split('$$$')[0];
+    codes = quiz.quiz.split('$$$')[1];
+
+    switch (quiz.category2.substring(1, 0)) {
+      case '1':
+        language = 'c';
+        break;
+      case '2':
+        language = 'cpp';
+        break;
+      case '3':
+        language = 'java';
+        break;
+      case '4':
+        language = 'python';
+        break;
+      default:
+        language = 'sql';
+        break;
+    }
+  }
 
   // 정답률
   const correctRate = isNaN(quiz.solvedNumber / quiz.answeredNumber)
@@ -71,7 +103,7 @@ const Paper = ({ quiz, quizLength, fetchQuiz }: Props) => {
         조합 키 TODO 조합키는 전역에 걸던가 해야될 것 같음. 또는 퀴즈쪽에만이라도.
      */
     // ALT + → (우측 화살표) 키
-    if (e.altKey && e.keyCode === 39) {
+    if (e.ctrlKey && e.keyCode === 39) {
       fetchQuiz();
       return;
     }
@@ -94,19 +126,25 @@ const Paper = ({ quiz, quizLength, fetchQuiz }: Props) => {
     const inputAnswer = answer.replace(' ', '').trim().toLowerCase();
     console.log(inputAnswer);
     if (answers.includes(inputAnswer)) {
-      // 정답 이벤트 기본 세팅
+      // 공통. TODO alert는 추후 수정
       alert('Answered!');
       setAnswer('');
       // 정답 분기
-      // TODO 푼 문제 + 1, 맞은 문제 + 1 update
-      solvedQuiz(quiz.id, true).then((r) => console.log);
+      solvedQuiz(quiz.id, true).then((r) => console.log); // 푼 문제 + 1 / 맞은 문제 + 1
+
+      // TODO 로그인한 유저 추가 체크
+
+      // 새로운 문제 로딩
       fetchQuiz();
     } else {
+      // 공통. TODO alert는 추후 수정
       alert('failed!');
       setAnswer('');
+
       // 실패 분기
-      // TODO 푼 문제 + 1
       solvedQuiz(quiz.id, false).then((r) => console.log);
+
+      // TODO 로그인한 유저 추가 체크
     }
   };
 
@@ -136,7 +174,7 @@ const Paper = ({ quiz, quizLength, fetchQuiz }: Props) => {
           {`단답형 ${quiz.id}.`}
         </Typography>
         <Typography variant='h5' component='h2'>
-          {`${category1Map.get(quiz.category1)} ${quiz.category2 ?? ''}`}
+          {`${category1Map.get(quiz.category1)} - ${quiz.category2 ?? ''}`}
         </Typography>
         {quiz.answeredNumber > 0 ? (
           <Typography className={classes.pos} color='textSecondary'>
@@ -146,7 +184,28 @@ const Paper = ({ quiz, quizLength, fetchQuiz }: Props) => {
           <Typography className={classes.pos} color='textSecondary' />
         )}
         <Typography variant='body2' component='p'>
-          {quiz.quiz}
+          {/* SQL, 프로그래밍 문제의 경우 */}
+          {['08', '10'].includes(quiz.category1) ? (
+            <>
+              <div>{quizContext}</div>
+              <CopyBlock
+                text={codes}
+                language={language}
+                theme={monokai}
+                wrapLines
+              />
+            </>
+          ) : (
+            // 일반 문제의 경우
+            quiz.quiz.split('\n').map((line) => {
+              return (
+                <>
+                  {line}
+                  <br />
+                </>
+              );
+            })
+          )}
         </Typography>
       </CardContent>
       <CardActions>
